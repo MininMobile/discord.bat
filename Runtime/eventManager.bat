befd1b35b4e278c773edc0d0ed96e53bb802d389
MininMobile <miningamer@gmail.com>
2017-11-04 18:24:43 +0000

    Improved event system

---
+++ Runtime/eventManager.bat
@@ -0,0 +1,13 @@
+call ..\Config\commands.bat
+
+:trigger :: Arguments = (eventName, eventInfo)
+if %~1 == message call:message "%~1"
+
+GOTO:EOF
+
+:message :: Arguments = (messageContent)
+set messageContent=%~1
+set messageContent=%messageContent:~1%
+call ..\Config\commands.bat
+call:%messageContent%
+goto:eof
