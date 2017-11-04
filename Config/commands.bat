bb1e8643a1dfeba55fa88063f894a377cf842532
MininMobile <miningamer@gmail.com>
2017-11-04 18:28:57 +0000

    Better reply method

--- Config/commands.bat
+++ Config/commands.bat
@@ -1,1 +1,11 @@
-set commands=help ping credits
+:help
+call:sendMessage "Commands: $help, $ping, $info"
+goto:eof
+
+:ping
+call:sendMessage "Pong"
+goto:eof
+
+:info
+call:sendMessage "Made using `discord.bat` (Yes, it's real)"
+goto:eof
