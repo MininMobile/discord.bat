6a21c9d17753379dd704562e1abf1d85bd3bc71f
MininMobile <miningamer@gmail.com>
2017-11-06 18:12:34 +0000

    changed things lol

--- Runtime/eventManager.bat
+++ Runtime/eventManager.bat
@@ -7,15 +7,15 @@ goto exit
 
 :trigger :: Arguments = (eventName, eventInfo)
 if %~1 == message call:message "%~2"
-GOTO:EOF
+goto:eof
 
 :message :: Arguments = (messageContent)
 if %events.message.notification% == 1 (
     echo Recieved Message: %~1
 )
 
-SET firstChar=%~1
-SET firstChar=!firstChar:~0,1!
+set firstChar=%~1
+set firstChar=!firstChar:~0,1!
 if !firstChar! neq %bot.prefix% goto:eof
 
 set messageContent=%~1
